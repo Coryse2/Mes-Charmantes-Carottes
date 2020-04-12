2f35c552a0615bcce099e1441b62dd9170ed3c85
Coryse2 <coryse.quibel@gmail.com>
2020-04-12 23:33:35 +0200

    Garden & Vegetable crud

--- src/Controller/GardenController.php
+++ src/Controller/GardenController.php
@@ -2,18 +2,99 @@
 
 namespace App\Controller;
 
+use App\Entity\Garden;
+use App\Form\GardenType;
+use App\Repository\GardenRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/garden")
+ */
 class GardenController extends AbstractController
 {
     /**
-     * @Route("/garden", name="garden")
+     * @Route("/", name="garden_index", methods={"GET"})
      */
-    public function index()
+    public function index(GardenRepository $gardenRepository): Response
     {
         return $this->render('garden/index.html.twig', [
-            'controller_name' => 'GardenController',
+            'gardens' => $gardenRepository->findAll(),
         ]);
+    }
+
+    /**
+     * @Route("/new", name="garden_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $garden = new Garden();
+        $form = $this->createForm(GardenType::class, $garden);
+        $form->handleRequest($request);
+        $id = $this->getUser()->getId();
+        
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // On associe le user connecté à l'annonce
+            $garden->setUser($this->getUser($id));
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($garden);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('garden_show', [ 
+                'id' => $garden->getId(),
+                ]);
+            }
+
+        return $this->render('garden/new.html.twig', [
+            'garden' => $garden,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="garden_show", methods={"GET"})
+     */
+    public function show(Garden $garden): Response
+    {
+        return $this->render('garden/show.html.twig', [
+            'garden' => $garden,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="garden_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Garden $garden): Response
+    {
+        $form = $this->createForm(GardenType::class, $garden);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('garden_index');
+        }
+
+        return $this->render('garden/edit.html.twig', [
+            'garden' => $garden,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="garden_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Garden $garden): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$garden->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($garden);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('garden_index');
     }
 }
